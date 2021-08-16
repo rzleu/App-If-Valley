fd6de607f30e2eecdca6889301284dcce9a2cc11
Richard Leung <richardzleung@gmail.com>
2021-08-15 20:45:09 -0700

    config

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(mp3)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 }
